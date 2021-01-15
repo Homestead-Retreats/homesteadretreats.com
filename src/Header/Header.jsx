c3dec25bc5a4df30532a35c8e09c561dffbffa0d
Nathan Wigen <nathan.s.wigen@hotmail.com>
2021-01-15 09:28:59 -0800

    fixed header button text color

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,26 +5,26 @@ import './Header.css'
 export default function Header() {
   return (
     <div className="header-links">
-      <div>
-        <Link to="/" className="header-link">ABOUT US</Link>
+      <div className="header-link-parent">
+        <Link to="/" className="header-link-home">ABOUT US</Link>
       </div>
-      <div>
-        <Link to="/facilities-food" className="header-link">FACILITIES AND FOOD</Link>
+      <div className="header-link-parent">
+        <Link to="/facilities-food" className="header-link-facilities">FACILITIES AND FOOD</Link>
       </div>
-      <div>
-        <Link to="/property" className="header-link">PROPERTY</Link>
+      <div className="header-link-parent">
+        <Link to="/property" className="header-link-property">PROPERTY</Link>
       </div>
-      <div>
-        <Link to="/onsite-recreation" className="header-link">ONSITE RECREATION</Link>
+      <div className="header-link-parent">
+        <Link to="/onsite-recreation" className="header-link-onsite">ONSITE RECREATION</Link>
       </div>
-      <div>
-        <Link to="/offsite-recreation" className="header-link">OFFSITE RECREATION</Link>
+      <div className="header-link-parent">
+        <Link to="/offsite-recreation" className="header-link-offsite">OFFSITE RECREATION</Link>
       </div>
-      <div>
-        <Link to="/rental" className="header-link">RENTAL INFORMATION</Link>
+      <div className="header-link-parent">
+        <Link to="/rental" className="header-link-rental">RENTAL INFORMATION</Link>
       </div>
-      <div>
-        <Link to="/floor-plan" className="header-link">FLOOR PLANS</Link>
+      <div className="header-link-parent">
+        <Link to="/floor-plan" className="header-link-floor">FLOOR PLANS</Link>
       </div>
     </div>
   )
